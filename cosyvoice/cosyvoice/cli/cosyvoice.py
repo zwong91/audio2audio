b81e7a911626cf380cdc38429ed31f8d2058e003
jong-un <36259838+zwong91@users.noreply.github.com>
2024-11-13 19:32:42 +0800

    Revert "Update cosyvoice.py"
    This reverts commit 4eedf6b9227b414d6be28f9e4c2c89290dccaf82.

--- cosyvoice/cosyvoice/cli/cosyvoice.py
+++ cosyvoice/cosyvoice/cli/cosyvoice.py
@@ -23,7 +23,7 @@ from cosyvoice.utils.file_utils import logging
 
 class CosyVoice:
 
-    def __init__(self, model_dir, load_jit=True, load_onnx=False, fp16=True):
+    def __init__(self, model_dir, load_jit=False, load_onnx=True, fp16=False):
         instruct = True if '-Instruct' in model_dir else False
         self.model_dir = model_dir
         if not os.path.exists(model_dir):
