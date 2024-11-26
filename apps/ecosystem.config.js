3b9acb0a12f4dfbf137358b7eeab92a1d26cd147
jong-un <36259838+zwong91@users.noreply.github.com>
2024-11-26 18:12:11 +0800

    更新生态系统配置文件，修改工作目录路径以反映项目结构变更

--- apps/ecosystem.config.js
+++ apps/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: "rt-audio",
         script: "app_ws.py",
-        cwd: "/workspaces/audio2audio/voice_chat", // 替换为你的项目目录
+        cwd: "/workspaces/rt-audio/voice_chat", // 替换为你的项目目录
         interpreter: "/home/ubuntu/miniconda3/envs/chattts/bin/python", // 替换为你的 conda 环境中的 Python 解释器路径
         env: {
           CONDA_DEFAULT_ENV: "chattts", // 替换为你的 conda 环境名称
