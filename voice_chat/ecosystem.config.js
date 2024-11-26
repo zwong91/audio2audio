83d1d5dc4d6c2954b708f952df98b2121810f7fe
jong-un <36259838+zwong91@users.noreply.github.com>
2024-11-26 15:25:39 +0800

    更新ecosystem.config.js，简化rt-audio应用配置并调整脚本路径

--- voice_chat/ecosystem.config.js
+++ voice_chat/ecosystem.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-  apps: [
-    {
-      name: "rt-audio",
-      script: "/home/ubuntu/miniconda3/envs/chattts/bin/uvicorn",
-      args: "app_ws:app --host 0.0.0.0 --port 5555 --ssl-keyfile cf.key --ssl-certfile cf.pem --log-level debug",
-      cwd: "/workspaces/audio2audio/voice_chat",
-      interpreter: "/home/ubuntu/miniconda3/envs/chattts/bin/python",
-      env: {
-        CONDA_DEFAULT_ENV: "chattts",
-        OPENAI_API_KEY: "sk-xxxxx",
+    apps: [
+      {
+        name: "rt-audio",
+        script: "app_ws.py",
+        cwd: "/workspaces/audio2audio/voice_chat", // 替换为你的项目目录
+        interpreter: "/home/ubuntu/miniconda3/envs/chattts/bin/python", // 替换为你的 conda 环境中的 Python 解释器路径
+        env: {
+          CONDA_DEFAULT_ENV: "chattts", // 替换为你的 conda 环境名称
+          OPENAI_API_KEY: "sk-xxxx"  // 替换为你的 OpenAI API Key
+        },
       },
-    },
-  ],
-};
+    ],
+  };
