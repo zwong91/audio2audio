f694153e2a9dc1c63b15ef4e0b8fa6ce92f9de38
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-02 17:25:27 +0800

    在 server.py 中移除线程池的创建和相关异步调用，简化音频处理逻辑

--- src/server.py
+++ src/server.py
@@ -13,8 +13,6 @@ from fastapi.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
 from starlette.middleware.cors import CORSMiddleware
 
-import concurrent.futures
-
 from src.client import Client
 
 class Server:
@@ -48,8 +46,16 @@ class Server:
         self.keyfile = keyfile
         self.connected_clients = {}
         
+        # Initialize FastAPI app
         self.app = FastAPI()
-
+        # 配置 CORS 中间件
+        self.app.add_middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
         # Add HTTP routes (like rendering HTML)
         self.templates = Jinja2Templates(directory="templates")
 
@@ -62,9 +68,6 @@ class Server:
            
         # Add WebSocket route for audio transcription
         self.app.websocket("/transcribe")(self.websocket_endpoint)
-        
-        # 创建线程池
-        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count() * 2)  # 根据需要调整线程池大小
 
     async def startup(self):
         """Called on startup to set up additional services."""
@@ -106,16 +109,9 @@ class Server:
                 break
 
     async def _process_audio(self, client, websocket):
-        loop = asyncio.get_event_loop()
-        
-        # 在线程池中异步执行处理任务
-        result = await loop.run_in_executor(self.executor, self._process_audio_sync, client, websocket)
-        return result
-
-    def _process_audio_sync(self, client, websocket):
-        # 这个方法将在线程池中执行
         try:
-            client.process_audio(
+            # 异步执行音频处理
+             client.process_audio(
                 websocket, self.vad_pipeline, self.asr_pipeline, self.llm_pipeline, self.tts_pipeline
             )
         except RuntimeError as e:
@@ -163,7 +159,7 @@ class Server:
             ssl_keyfile=self.keyfile,
             loop="uvloop",
             log_level="debug",
-            workers=os.cpu_count() * 2,
+            workers=os.cpu_count(),
             limit_concurrency=1000,
             limit_max_requests=10000,
             backlog=2048
