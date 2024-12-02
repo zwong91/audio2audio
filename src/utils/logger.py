23210a665c779ed2ac65933538b8e02490c9249f
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-02 18:23:00 +0800

    在 TimingLogger 中添加 warning 方法以增强日志记录功能

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -17,6 +17,9 @@ class TimingLogger:
     
     def error(self, message: str):
         self.logger.error(message)
+
+    def warning(self, message: str):
+        self.logger.warning(message)
     
     def info(self, message: str):
         self.logger.info(message)
