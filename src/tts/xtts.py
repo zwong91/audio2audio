77bfda6d6dd4775ee9e65196d3b42db031e1ddde
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-03 14:55:45 +0800

    更新 XTTS 类以使用新的模型名称，调整语音转换方法参数；更改 WebSocket 服务器地址端口为 8765，并更新音频文件路径

--- src/tts/xtts.py
+++ src/tts/xtts.py
@@ -12,7 +12,7 @@ from src.xtts.TTS.api import TTS
 class XTTS(TTSInterface):
     def __init__(self, voice: str = ''):
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+        self.tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts", progress_bar=False).to(device)
 
     async def text_to_speech(self, text: str) -> Tuple[str, str]:
       
@@ -20,9 +20,9 @@ class XTTS(TTSInterface):
         target_wav = os.path.join(os.path.abspath(os.path.join(os.getcwd(), "../rt-audio/vc")), "liuyifei.wav")
         speech_file_path = f"/tmp/audio_{uuid4()}.wav"
         # 调用语音转换方法
-        self.tts.tts_with_vc_to_file(
+        self.tts.tts_to_file(
             text,
-            speaker_wav=target_wav,
+            speaker=target_wav,
             language="zh-CN", 
             file_path=speech_file_path
         )
