c8276b5444fbba67eae1fc4e0776a7d3b6bab453
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-03 17:07:52 +0800

    更新 EdgeTTS 类的默认语音参数，将其从 'zh-CN-XiaoxiaoNeural' 修改为 'zh-TW-HsiaoChenNeural'

--- src/tts/edge_tts.py
+++ src/tts/edge_tts.py
@@ -6,7 +6,8 @@ import edge_tts
 from .tts_interface import TTSInterface
 
 class EdgeTTS(TTSInterface):
-    def __init__(self, voice: str = 'zh-CN-XiaoxiaoNeural'):
+    #edge-tts --list-voices
+    def __init__(self, voice: str = 'zh-TW-HsiaoChenNeural'):
         self.voice = voice
 
     async def text_to_speech(self, text: str, rate: str = '50%', pitch: str = '-50Hz', volume: int = 70) -> Tuple[str, str]:
