03977cb83e48b171d550109f04ee29f30c7a6064
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-03 18:31:46 +0800

    更新默认系统消息中的角色名称为晓晓，并修改 EdgeTTS 类的默认语音为 zh-CN-XiaoxiaoNeural

--- src/tts/edge_tts.py
+++ src/tts/edge_tts.py
@@ -7,7 +7,7 @@ from .tts_interface import TTSInterface
 
 class EdgeTTS(TTSInterface):
     #edge-tts --list-voices
-    def __init__(self, voice: str = 'zh-TW-HsiaoChenNeural'):
+    def __init__(self, voice: str = 'zh-CN-XiaoxiaoNeural'):
         self.voice = voice
 
     async def text_to_speech(self, text: str, rate: str = '50%', pitch: str = '-50Hz', volume: int = 70) -> Tuple[str, str]:
