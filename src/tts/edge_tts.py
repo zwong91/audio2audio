fc5a532df6228c460e7f135f0ffa5450b023aee8
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-03 16:35:19 +0800

    更新 text_to_speech 方法的参数类型，将语速从字符串改为整数，以支持更精确的控制

--- src/tts/edge_tts.py
+++ src/tts/edge_tts.py
@@ -15,7 +15,7 @@ class EdgeTTS(TTSInterface):
         for voice in voices:
             print(f"Voice Name: {voice['Name']}, Gender: {voice['Gender']}, Language: {voice['Locale']}")
 
-    async def text_to_speech(self, text: str, rate: str = '50%', pitch: str = '10%', volume: int = 70) -> Tuple[str, str]:
+    async def text_to_speech(self, text: str, rate: int = 20, pitch: str = '10%', volume: int = 70) -> Tuple[str, str]:
         """使用 edge_tts 库将文本转语音，并设置语速、音调和音量"""
         temp_file = f"/tmp/audio_{uuid4().hex[:8]}.mp3"
         communicate = edge_tts.Communicate(
