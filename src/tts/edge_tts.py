f771b404af0d7715f9b91d28a33d7e3254f79b0d
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-05 03:28:32 +0800

    Update edge_tts.py

--- src/tts/edge_tts.py
+++ src/tts/edge_tts.py
@@ -1,6 +1,4 @@
-import os
 import time
-from uuid import uuid4
 from typing import Tuple
 import edge_tts
 from io import BytesIO
@@ -13,17 +11,14 @@ class EdgeTTS(TTSInterface):
     async def text_to_speech(self, text: str, rate: str = '50%', pitch: str = '-50Hz', volume: int = 70) -> Tuple[bytes, str, str]:
         start_time = time.time()
         """使用 edge_tts 库将文本转语音"""
-        temp_file = f"/tmp/audio_{uuid4().hex[:8]}.mp3"
+        
         communicate = edge_tts.Communicate(text=text, voice=self.voice)
-        await communicate.save(temp_file)
-
-        # 将音频文件保存到内存缓冲区
-        audio_file = BytesIO()
-        with open(temp_file, 'rb') as f:
-            audio_file.write(f.read())
-        audio_file.seek(0)
+        audio_bytes = BytesIO()
+        async for chunk in communicate.stream():
+            audio_bytes.write(chunk)
+        audio_bytes.seek(0)
 
         end_time = time.time()
         print(f"EdgeTTS text_to_speech time: {end_time - start_time:.4f} seconds")
 
-        return audio_file.read(), text, os.path.basename(temp_file)
+        return audio_bytes.read(), text, "in_memory_audio.mp3"
