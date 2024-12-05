44d6aa32460a0bfe7daf65d46953189d4433a372
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-05 22:35:56 +0800

    更新 page.tsx 1.5發送一次音頻塊

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -189,7 +189,7 @@ export default function Home() {
                 type: 'audio',
                 recorderType: StereoAudioRecorder,
                 mimeType: 'audio/wav',
-                timeSlice: 600,
+                timeSlice: 1500,
                 desiredSampRate: 16000,
                 numberOfAudioChannels: 1,
                 ondataavailable: (blob: Blob) => {
