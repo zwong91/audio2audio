7c9930e1c42be19d5b449a149db0a1009fe0ae67
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-07 06:07:46 +0800

    Update tts_factory.py

--- src/tts/tts_factory.py
+++ src/tts/tts_factory.py
@@ -1,5 +1,5 @@
 from .edge_tts import EdgeTTS
-from .xtts import XTTS, XTTSv2
+from .xtts import XTTS
 class TTSFactory:
     @staticmethod
     def create_tts_pipeline(asr_type, **kwargs):
@@ -7,7 +7,5 @@ class TTSFactory:
             return EdgeTTS(**kwargs)
         elif asr_type == "xtts":
             return XTTS(**kwargs)
-        elif asr_type == "xttsv2":
-            return XTTSv2(**kwargs)
         else:
             raise ValueError(f"Unknown TTS pipeline type: {tts_type}")
