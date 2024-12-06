82667fd59e855b68a591b7ad024807d14ea63d79
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-06 17:35:41 +0800

    重构应用启动配置，新增 rt-audio-edge 和 rt-audio-vc 服务，删除旧的启动脚本

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,25 @@
 module.exports = {
   apps: [
     {
-      name: "rt-audio",
-      script: "./start_app.sh",
-      cwd: "/home/ubuntu/proj/rt-audio", // 替换为你的项目目录
-      interpreter: "/bin/bash", // 使用 Bash 解释器
+      name: "rt-audio-edge",
+      script: "./start_app_edge.sh",
+      cwd: "/home/ubuntu/front/rt-audio",
+      interpreter: "/bin/bash",
       env: {
-        CONDA_DEFAULT_ENV: "rt", // 替换为你的 conda 环境名称
-        OPENAI_API_KEY: "sk-xxxx"  // 替换为你的 OpenAI API Key
+        CONDA_DEFAULT_ENV: "rt",
+        OPENAI_API_KEY: "sk-xxxx" // 替换为你的 OpenAI API Key
       },
     },
+    {
+      name: "rt-audio-vc",
+      script: "./start_app_vc.sh",
+      cwd: "/home/ubuntu/front/rt-audio",
+      interpreter: "/bin/bash",
+      env: {
+        CONDA_DEFAULT_ENV: "rt",
+        OPENAI_API_KEY: "sk-xxxx"
+      },
+    },
+    // 可以继续添加更多的服务配置
   ],
 };
