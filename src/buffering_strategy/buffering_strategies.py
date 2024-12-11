37e518cffe1d9fc1014127f318ac5c1a18dd9c53
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-11 17:00:22 +0800

    "Error in realtime processing: tried processing a new "                     "chunk while the previous one was still being processed"

--- src/buffering_strategy/buffering_strategies.py
+++ src/buffering_strategy/buffering_strategies.py
@@ -77,6 +77,7 @@ class SilenceAtEndOfChunk(BufferingStrategyInterface):
         )
         if len(self.client.buffer) > chunk_length_in_bytes:
             if self.processing_flag or self.client.scratch_buffer:
+                print("Warning in realtime processing: tried processing a new chunk while the previous one was still being processed")
                 return
             self.client.scratch_buffer += self.client.buffer
             self.client.buffer.clear()
