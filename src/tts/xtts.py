1b68caf825e48b53fba85cde7f2f6e673d991531
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-12 01:26:19 +0800

    Update xtts.py

--- src/tts/xtts.py
+++ src/tts/xtts.py
@@ -16,12 +16,57 @@ import glob
 
 sys.path.insert(1, "../vc")
 
+import edge_tts
 from src.xtts.TTS.api import TTS
 from src.xtts.TTS.tts.configs.xtts_config import XttsConfig    
 from src.xtts.TTS.tts.models.xtts import Xtts
 
 from src.xtts.TTS.utils.generic_utils import get_user_data_dir
 from src.xtts.TTS.utils.manage import ModelManager
+
+class XTTS(TTSInterface):
+    def __init__(self, voice: str = 'zh-CN-XiaoxiaoNeural'):
+        self.voice = voice
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.tts = TTS(model_name="voice_conversion_models/multilingual/vctk/freevc24", progress_bar=False).to(device)
+
+    async def text_to_speech(self, text: str, rate: int = 0, pitch: int = 20, volume: int = 110) -> Tuple[bytes, str]:
+        audio_buffer = BytesIO()
+        """使用 x_tts 库将文本转语音"""
+        start_time = time.time()
+
+        temp_file = f"/tmp/audio_{uuid4().hex[:8]}.wav"
+        rate_str = f"{rate:+d}%"
+        pitch_str = f"{pitch:+d}Hz"
+        volume_str = f"{volume:+d}%"
+        communicate = edge_tts.Communicate(text=text, voice=self.voice, pitch=pitch_str, volume=volume_str)
+        await communicate.save(temp_file)
+
+        # 使用 os.path 确保路径正确拼接
+        target_wav = os.path.join(os.path.abspath(os.path.join(os.getcwd(), "../rt-audio/vc")), "liuyifei.wav")
+        speech_file_path = f"/tmp/audio_{uuid4().hex[:8]}.wav"
+
+        tts_task = asyncio.create_task(
+            asyncio.to_thread(
+                self.tts.voice_conversion_to_file,
+                source_wav=temp_file,
+                target_wav=target_wav,
+                file_path=speech_file_path
+            )
+        )
+        await tts_task
+
+        # 将生成的音频文件读入内存缓冲区
+        with open(speech_file_path, 'rb') as f:
+            audio_buffer.write(f.read())
+
+        audio_buffer.seek(0)
+        audio_data = audio_buffer.read()
+
+        end_time = time.time()
+        print(f"XTTS text_to_speech time: {end_time - start_time:.4f} seconds")
+
+        return audio_data, speech_file_path
 
 class XTTS_v2(TTSInterface):
     def __init__(self, voice: str = 'liuyifei'):
