62e9943cd4dcd217c6035ceaaec4b9e6e67647ee
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-11 16:18:06 +0800

    vault_embeddings_tensor

--- src/llm/openai_llm.py
+++ src/llm/openai_llm.py
@@ -45,17 +45,17 @@ class OpenAILLM(LLMInterface):
         self.vault_embeddings = self.embedding_model.encode(vault_content) if vault_content else []
 
     
-    def get_relevant_context(self, user_input, vault_embeddings, top_k=3):
+    def get_relevant_context(self, user_input, vault_embeddings_tensor, top_k=3):
         """
         Retrieves the top-k most relevant context from the vault based on the user input.
         Local RAG embedding search
         """
-        if vault_embeddings.nelement() == 0: # Check if the tensor has any elements
+        if vault_embeddings_tensor.nelement() == 0: # Check if the tensor has any elements
             return []
         # Encode the user input
         input_embedding = self.embedding_model.encode([user_input])
         # Compute cosine similarity between the input and vault embeddings
-        cos_scores = util.cos_sim(torch.tensor(input_embedding).unsqueeze(0), vault_embeddings)
+        cos_scores = util.cos_sim(torch.tensor(input_embedding), vault_embeddings_tensor)
         # Adjust top_k if it's greater than the number of available scores
         top_k = min(top_k, len(cos_scores))
         # Sort the scores and get the top-k indices
