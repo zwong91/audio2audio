d294259a982f2fc9067e68d68c816beea7969889
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-12 15:14:12 +0800

    do_sample=False,

--- src/tts/xtts.py
+++ src/tts/xtts.py
@@ -131,8 +131,8 @@ class XTTS_v2(TTSInterface):
             length_penalty=1.0,
             repetition_penalty=10.0,
             top_k=10,
-            top_p=0.9,
-            do_sample=True,
+            top_p=0.1,
+            do_sample=False,
             speed=1.0,
             enable_text_splitting=True,
         )
